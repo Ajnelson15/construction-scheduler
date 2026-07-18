be49f45f85c1503191ebfb073dd17951214a9b5a
Andrew Nelson <ajnelsoncontracting@gmail.com>
2026-07-18 12:42:54 -0500

    Created construction task model

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,6 +1,8 @@
 package app;
 
+import model.ConstructionTask;
 import model.TaskStatus;
+import java.time.LocalDate;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -9,5 +11,11 @@ public class Main {
         TaskStatus.ProjectStatus status = TaskStatus.ProjectStatus.IN_PROGRESS;
 
         System.out.println(status);
+
+
+        ConstructionTask framingTask = new ConstructionTask(1,"Andrew",LocalDate.of(2026,07,18), LocalDate.of(2026,07,25),1);
+
+        System.out.println(framingTask.getTask_name() + "\n"+ framingTask.getTask_id() + "\n" + framingTask.getStart_date());
+
     }
 }
--- src/model/ConstructionTask.java
+++ src/model/ConstructionTask.java
@@ -1,4 +1,59 @@
 package model;
 
+import java.time.LocalDate;
+
 public class ConstructionTask {
+    private int task_id;
+    private String task_name;
+    private LocalDate start_date;
+    private LocalDate end_date;
+    private int task_status;
+
+    public ConstructionTask(int task_id, String task_name, LocalDate start_date,LocalDate end_date, int task_status) {
+        this.task_id = task_id;
+        this.task_name = task_name;
+        this.start_date = start_date;
+        this.end_date = end_date;
+        this.task_status = task_status;
+    }
+    public int getTask_id() {
+        return task_id;
+    }
+
+    public String getTask_name() {
+        return task_name;
+    }
+
+    public String setTask_name(String task_name) {
+        return this.task_name = task_name;
+    }
+
+    public LocalDate getStart_date() {
+        return start_date;
+    }
+
+    public LocalDate setStart_date(LocalDate start_date) {
+        return this.start_date = start_date;
+    }
+
+    public LocalDate getEnd_date() {
+        return end_date;
+    }
+
+    public LocalDate setEnd_date(LocalDate end_date) {
+        return this.end_date = end_date;
+    }
+
+    public int getTask_status () {
+        return task_status;
+    }
+
+    public int setTask_status() {
+        return this.task_status = task_status;
+    }
 }
+
+
+
+
+
